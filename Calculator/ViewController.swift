9f901e7cae3c97c4c53922322558f3fe9147a605
Carrie(cat1784) <cat1784@g.rit.edu>
2017-01-17 15:29:35 -0500

    Finished Swift Calculator app :)

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,7 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var numDisplay:Double = 0
+    var prevNum:Double = 0
+    var preformingMath = false
+    var operation = 0
+    
+    @IBOutlet weak var output: UILabel!
 
+    
+    
+    @IBAction func num(sender: UIButton) {
+        if preformingMath == true
+        {
+            output.text = String(sender.tag-1)
+            numDisplay = Double(output.text!)!
+            preformingMath = false
+        }
+        else{
+            output.text = output.text! + String(sender.tag-1)
+            numDisplay = Double(output.text!)!
+        }
+    }
+    
+    @IBAction func functions(sender: UIButton) {
+        
+        if output.text != "" && sender.tag != 11 && sender.tag != 16{
+            prevNum = Double(output.text!)!
+            if sender.tag == 12 //Divide
+            {
+                output.text = "/";
+            }
+            else if sender.tag == 13 //Multiply
+            {
+                output.text = "*";
+            }
+            else if sender.tag == 14 //Subtract
+            {
+                output.text = "-";
+            }
+            else if sender.tag == 15 //Add
+            {
+                output.text = "+";
+            }
+            operation = sender.tag
+            preformingMath = true
+        }
+        else if sender.tag == 16{
+            if operation == 12{
+                output.text = String(prevNum / numDisplay)
+            }
+            else if operation == 13{
+                output.text = String(prevNum * numDisplay)
+            }
+            else if operation == 14{
+                output.text = String(prevNum - numDisplay)
+            }
+            else if operation == 15{
+                output.text = String(prevNum + numDisplay)
+            }
+        }
+        else if sender.tag == 11
+        {
+            output.text = " "
+            prevNum = 0;
+            numDisplay = 0;
+            operation = 0;
+            
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
